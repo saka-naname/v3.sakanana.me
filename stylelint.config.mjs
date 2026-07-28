eba4ee02e2ff484c2e7864f01728c140efae6ff0
saka-naname <reisuicupsoup@gmail.com>
2026-07-29 02:52:36 +0900

    chore: add stylelint

---
+++ stylelint.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import("stylelint").Config} */
+export default {
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-html/astro",
+    "stylelint-config-recess-order",
+  ],
+};
